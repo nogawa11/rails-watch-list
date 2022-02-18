227701fd70d12918c33e488f79e2d8699ef3ef7f
Nicole Ogawa <nogawa11@gmail.com>
2022-02-18 11:20:18 +0900

    change platform in gemfile.lock

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,4 @@ class List < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
-  validates :image_url, presence: true
 end
